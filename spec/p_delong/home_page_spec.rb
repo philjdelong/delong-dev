aa06cbb764dc31d5dd57edac334a1dcd525c7809
Phil DeLong <philjdelong@gmail.com>
2020-04-27 13:31:58 -0600

    Write shell for initial test. Passes RSpec

---
+++ spec/p_delong/home_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "As a user" do
+	describe "when i visit the home page", type: :feature do
+		it "i see a profile image" do
+			
+		end
+
+		it "i see a nav with home about background education" do
+			
+		end
+	end
+end
