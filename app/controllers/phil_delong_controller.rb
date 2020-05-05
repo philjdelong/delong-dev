5934c10a20b023c5a7d90cd170c087bb77c20ed2
Phil DeLong <philjdelong@gmail.com>
2020-05-05 12:23:02 -0600

    Add controller methods

--- app/controllers/phil_delong_controller.rb
+++ app/controllers/phil_delong_controller.rb
@@ -1,5 +1,16 @@
 class PhilDelongController < ApplicationController
+	def about
+	end
 
-	def index
+	def career
+	end
+
+	def education
+	end
+
+	def pdf
+		resume = File.join(Rails.root, "app/assets/images/delong_resume.pdf")
+		# inserting 'disposition' below renders the image, removing 'disposition will download the file immediately'
+		send_file(resume, :filename => "delong_resume.pdf", :disposition => 'inline', :type => "application/pdf")
 	end
 end
