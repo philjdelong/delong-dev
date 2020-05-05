c1438ac9a63e02f0007b101e4e4e34235ba24a95
Phil DeLong <philjdelong@gmail.com>
2020-05-05 12:23:31 -0600

    Update views and include pdf route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/", to: "phil_delong#index"
-  get "/projects", to: "projects#index"
-  get "/career", to: "career#index"
-  get "/education", to: "education#index"
+  get "/", to: "phil_delong#about"
+  get "/projects", to: "phil_delong#projects"
+  get "/career", to: "phil_delong#career"
+  get "/education", to: "phil_delong#education"
+  
+  get "/resume", to: "phil_delong#resume"
+  get "/pdf", to: "phil_delong#pdf"
 end
