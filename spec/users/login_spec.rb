3774dda060899e17235ad543d57185d73f25d6b3
Phil DeLong <philjdelong@gmail.com>
2020-05-11 11:01:01 -0600

    Create spec for user login

---
+++ spec/users/login_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "As a user" do
+	it "i can login" do
+		click_link "Register/Login"
+		fill_in "email", with: "philjdelong@gmail.com"
+		expect(current_path).to eq("/confirm")
+
+		fill_in "password", with: "password"
+		click_button "Login"
+		expect(current_path).to eq("/")
+		expect(page).to have_content("Welcome, Phil!")
+	end
+end
