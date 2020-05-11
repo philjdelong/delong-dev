3c5f21833d5953a5108acdd7e64df8695939fbb6
Phil DeLong <philjdelong@gmail.com>
2020-05-11 10:04:27 -0600

    Update home page test

--- spec/p_delong/home_page_spec.rb
+++ spec/p_delong/home_page_spec.rb
@@ -7,14 +7,21 @@ RSpec.describe "As a user" do
 		end
 
 		it "i see a profile image" do
-			expect(page).to have_css("#profile_image")
+			expect(page).to have_css(".profile_image")
 		end
 
-		it "i see a nav with home about background education" do
-			expect(page).to have_link('projects')
-			expect(page).to have_link('education')
-			expect(page).to have_link('career')
-			expect(page).to have_link('calendar')
+		it "i see a nav with three links" do
+			expect(page).to have_link('Home')
+			expect(page).to have_link('Projects')
+			expect(page).to have_link('Resume')
+		end
+
+		it "i see About heading and Early Childhood Career and Meet the Gang sub-headings" do
+			expect(page).to have_content("About")
+			expect(page).to have_content("Childhood and Education")
+			expect(page).to have_content("Career")
+			expect(page).to have_content("Transition into Software")
+			expect(page).to have_content("Meet the Gang")
 		end
 	end
 end
