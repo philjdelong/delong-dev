6e133c1c7168dee2f9c89a683d90e5b4578fb7e4
Phil DeLong <philjdelong@gmail.com>
2020-05-12 14:18:12 -0600

    Create method for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
 	def new
-		
+	end
+
+	def create
+		user = User.create(user_params)
+		user.save
+		if user.save
+			session[:user_id] = user.id
+			redirect_to "/"
+		else
+			redirect_to "/register"
+		end
+	end
+
+	private
+
+	def user_params
+		params.permit(:username, :email, :password)
 	end
 end
