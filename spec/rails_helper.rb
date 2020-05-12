277bb6f59eebf44078936baa83b01da5a9a8055f
Phil DeLong <philjdelong@gmail.com>
2020-05-12 14:18:38 -0600

    Include bcrypt and shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
+
 require 'pry'
+
+require 'bcrypt'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
@@ -65,4 +68,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
