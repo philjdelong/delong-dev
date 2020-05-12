7081fa4f67244f0ba2ab283399a0f4a3be329447
Phil DeLong <philjdelong@gmail.com>
2020-05-12 15:18:07 -0600

    Add flash message types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	helper_method :current_user
+	add_flash_types :info, :error, :message
 
 	def current_user
 		user ||= User.find(session[:user_id]) if session[:user_id]
