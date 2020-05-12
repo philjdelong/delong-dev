7f973df548467d3dc69cc6f4f922c2bd46805b6a
Phil DeLong <philjdelong@gmail.com>
2020-05-12 15:18:53 -0600

    Include flash messages for registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
 		user = User.create(user_params)
 		user.save
 		if user.save
+			flash[:success] = "Registration complete!"
 			session[:user_id] = user.id
 			redirect_to "/"
 		else
+			flash[:error] = "Please enter a unique username and complete all fields."
 			redirect_to "/register"
 		end
 	end
