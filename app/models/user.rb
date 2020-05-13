82e09f31dbbf791f2caf3fd9b70fcea83ca9f454
Phil DeLong <philjdelong@gmail.com>
2020-05-13 12:08:53 -0600

    Update user to validate unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	validates :username, uniqueness: true
+	validates :email, uniqueness: true
 	validates_presence_of :username, :email, :password
 
 	has_secure_password
