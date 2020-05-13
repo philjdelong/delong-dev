813b527a35c0f083351e5a7c40f562ac9cb0082d
Phil DeLong <philjdelong@gmail.com>
2020-05-13 13:01:37 -0600

    Include enum for :role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 	validates :username, uniqueness: true
 	validates :email, uniqueness: true
-	validates_presence_of :username, :email, :password
+	validates_presence_of :username, :email, :password, :role
 
 	has_secure_password
+	enum role: %w(default admin)
 end
