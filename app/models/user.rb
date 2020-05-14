edcee972975d9ff8f6fddd941df36362563ceba6
Phil DeLong <philjdelong@gmail.com>
2020-05-13 21:35:50 -0600

    Include password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	validates :username, uniqueness: true
 	validates :email, uniqueness: true
-	validates_presence_of :username, :email, :password, :role
+	validates_presence_of :username, :email, :password, :password_confirmation, :role
 
 	has_secure_password
 	enum role: %w(default admin)
