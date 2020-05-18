41195bad96829376baa6051a1618931cd5d8fd53
Phil DeLong <philjdelong@gmail.com>
2020-05-18 12:11:40 -0600

    Update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
 	validates :email, uniqueness: true
 	validates :slug, uniqueness: true
 	validates_presence_of :username, :email, :password, :password_confirmation, :role
-	after_validation :generate_slug, only: [:create]
 
+	
 	has_secure_password
 	enum role: %w(default admin)
-
+	
+	before_validation :generate_slug, only: [:create, :update]
 
 	private
 
 	def generate_slug
-		self.slug = username.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
+		self.slug = username.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
 	end
 end
