d52ee56e16e2396dbd3959200a2316345f025acb
Phil DeLong <philjdelong@gmail.com>
2020-05-20 14:43:33 -0600

    Add slug to legacy users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.all.each do |x|
+	unless x.slug
+		x.slug = x.username
+	end
+end
 
 User.create(
 	username: "Default",
