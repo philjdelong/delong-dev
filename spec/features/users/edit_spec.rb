9f99a547af78dcb95d00a846c20e22835c174987
Phil DeLong <philjdelong@gmail.com>
2020-05-20 13:24:33 -0600

    Pass edit spec

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "As a user", type: :feature do
 	describe "when i visit the dashboard" do
 		it "i can edit all of my information" do
-			username = "phil"
-			email = "example@email.com"
+			username = "user"
+			email = "user@email.com"
 			password = "password"
 	
 			user = User.create(
@@ -14,41 +14,29 @@ RSpec.describe "As a user", type: :feature do
 				password_confirmation: password
 			)
 
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
 			visit "/"
-
-			click_link "login"
-
-			fill_in "email:", with: email
-			fill_in "password:", with: password
-
-			click_button "Login"
-			expect(current_path).to eq("/")
-
+			
 			click_link "Dashboard"
-			expect(current_path).to eq("/#{user.slug}")
+			expect(current_path).to eq("/#{user.slug}/profile")
 
 			click_link "Edit"
 			expect(current_path).to eq("/#{user.slug}/edit")
 			
 			fill_in "email:", with: "new@email.com"
-			fill_in "username:", with: "New Username"
-			
+			fill_in "username:", with: "New User"
+			fill_in "password:", with: password
+			fill_in "password confirmation:", with: password
+		
 			click_button "Update"
-			expect(current_path).to eq("/#{user.slug}")
-
-
+			expect(current_path).to eq("/#{user.slug}/profile")
+			
+			user.reload
 			expect(user.email).to eq("new@email.com")
-			expect(user.username).to eq("New Username")
+			expect(user.username).to eq("New User")
 
-			within('nav') do
-				expect(page).to have_content("Welcome, New Username")
-			end
-
-			visit "/"
-
-			within('nav') do
-				expect(page).to have_content("Welcome, New Username")
-			end
+			expect(page).to have_content("Your information has been updated")
 		end
 	end
 end
