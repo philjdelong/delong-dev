074383f3d0aa89a92772f0390928c0c99cab83f1
Phil DeLong <philjdelong@gmail.com>
2020-05-20 14:50:56 -0600

    Update to map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.all.each do |x|
+User.all.map do |x|
 	unless x.slug
 		x.slug = x.username
 	end
