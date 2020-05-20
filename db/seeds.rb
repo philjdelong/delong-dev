af36a5a5af4c6589c10411ee84440903caa2c972
Phil DeLong <philjdelong@gmail.com>
2020-05-20 14:54:51 -0600

    Remove slug conditional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.all.map do |x|
-	unless x.slug
-		x.slug = x.username
-	end
-end
 
 User.create(
 	username: "Default",
