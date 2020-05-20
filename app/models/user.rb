0b1d36abf4b228d2b6fc9b0108999600fd950125
Phil DeLong <philjdelong@gmail.com>
2020-05-20 13:17:46 -0600

    Create conditional for get_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
 	validates :email, uniqueness: true
 	validates_presence_of :username, :email, :password, :password_confirmation, :role
-	before_validation :generate_slug, only: :create
-	
-	validates :slug, presence: true
+	before_validation :get_slug
+	validates :slug, uniqueness: true
 
 	has_secure_password
 	enum role: %w(default admin)
 
-	def generate_slug
-		self.slug = username.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
+	def get_slug
+		self.slug || self.slug = username.to_s.downcase.parameterize.tr("_", "") + "-" + rand(100_000).to_s(26)
 	end
 end
